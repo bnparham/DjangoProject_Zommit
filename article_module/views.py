c47786dc44ad4bf7393a486f3335d0bad1201cd0
bnparham <parhambn.workmail@gmail.com>
2022-12-17 20:59:05 +0330

    add some article to model and show them in template, create main-side component inside bodySection component

--- article_module/views.py
+++ article_module/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Article
 # Create your views here.
+
+class body_main_sideView(ListView):
+    model = Article
+    template_name = "shared/body_components/main_side.html"
+
+
